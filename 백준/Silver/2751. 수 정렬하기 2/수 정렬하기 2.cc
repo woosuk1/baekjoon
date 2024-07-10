a221542ce75167b7977a131547db51ba2644e7fb
woosuk1 <74864092+woosuk1@users.noreply.github.com>
2024-07-10 19:46:51 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 600 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <climits>
+#include <set>
+#include <stack>
+using namespace std;
+
+using ll = long long;
+
+int main() {
+
+	int n;
+
+	cin >> n;
+
+	vector<int> arr(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr.begin(), arr.end());
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << '\n';
+	}
+	return 0;
+}
