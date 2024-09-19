0cb791047df8668c91bff6c921983faad04169d8
woosuk1 <74864092+woosuk1@users.noreply.github.com>
2024-09-19 18:14:07 +0900

    [Silver II] Title: 나무 자르기, Time: 164 ms, Memory: 9712 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	long long k, n, cnt = 0, lines, mid =0;
+
+	long long arr[1000001];
+
+	cin >> k >> n;
+
+	for (int i = 0; i < k; i++) {
+		cin >> arr[i];
+		if (cnt < arr[i]) cnt = arr[i];
+	}
+
+	long long start = 1, end = cnt, res = 0;
+
+	while (start <= end) {
+
+		mid = (start + end) / 2;
+
+		lines = 0;
+		
+		long long temp = 0;
+
+		for (int i = 0; i < k; i++) {
+			
+			temp = arr[i] - mid;
+			if (temp < 0)
+				temp = 0;
+			lines += temp;
+		}
+
+		if (lines >= n) {
+			start = mid + 1;
+			if (res < mid) res = mid;
+		}
+		else
+			end = mid - 1;
+
+	}
+
+	cout << res << endl;
+}
