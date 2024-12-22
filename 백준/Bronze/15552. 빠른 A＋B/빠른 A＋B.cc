a76d6e31e5f5e3537b316fe5325bd7073e4a3e3d
Woosuk Ki <74864092+woosuk1@users.noreply.github.com>
2024-12-22 22:09:34 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 196 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <deque>
+#include <climits>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+using ll = long long;
+
+void init() {
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(false);
+}
+
+int main() {
+
+	init();
+
+	int n;
+
+	cin >> n;
+
+	int a, b;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		cout << a + b << '\n';
+	}
+
+	return 0;
+}
