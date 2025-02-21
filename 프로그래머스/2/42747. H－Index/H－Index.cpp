60abcba821627bcf2c15f04c50d39d4ff0444b85
Woosuk Ki <74864092+woosuk1@users.noreply.github.com>
2025-02-21 20:04:54 +0900

    [level 2] Title: H-Index, Time: 0.07 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    
+    sort(citations.begin(), citations.end());
+    
+    for(int i=0; i<citations.size(); i++){
+        if(citations.size() - i >= citations[i])
+            answer = citations[i];
+        else{
+            answer = max(answer, (int)citations.size() -i);
+        }
+    }
+    
+    return answer;
+}
