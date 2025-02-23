d09b171e04420fa4cf66cbfafd1119ed2e8adce7
Woosuk Ki <74864092+woosuk1@users.noreply.github.com>
2025-02-24 01:31:09 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -1,140 +1,106 @@
-#include<iostream>
+#include <iostream>
 #include <cstdio>
-#include <algorithm> // for max
+#include <algorithm>
 #include <vector>
 #include <string>
 #include <map>
 #include <limits.h>
 #include <queue>
+#include <set>
+#include <math.h>
+#include <stack>
+#include <deque>
+#include <tuple>
 
 using namespace std;
 
-bool visited[101][101];
-bool visited2[101][101];
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
 
-vector<vector <char>> arr;
-
-int dx[4] = {0,0,1,-1};
-int dy[4] = {1,-1,0,0};
-
-int n;
-
-// 적록색약이 아닌 경우
-void bfs(int startY, int startX) {
-
-	// 같아야 하는 변수
-	char target = arr[startY][startX];
-
-	queue<pair<int, int>> q;
-
-	visited[startY][startX] = true;
-	
-	q.push(make_pair(startY,startX));
-
-	while (!q.empty()) {
-		int y = q.front().first;
-		int x = q.front().second;
-
-		q.pop();
-
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if (nx < 0 || ny < 0 || nx >= n || ny >= n || arr[ny][nx] != target || visited[ny][nx]) {
-				continue;
-			}
-			visited[ny][nx] = true;
-			q.push(make_pair(ny, nx));
-		}
-	}
-}
-
-// 적록색약인 경우 (빨 = 초)
-void bfs2(int startY, int startX) {
-
-	// 같아야 하는 변수
-	char target = arr[startY][startX];
-
-	bool flag = false;
-
-	// 빨강, 초록 일시 flag 활성화
-	if (target == 'R' || target == 'G') {
-		flag = true;
-	}
-
-	queue<pair<int, int>> q;
-
-	visited[startY][startX] = true;
-
-	q.push(make_pair(startY, startX));
-
-	while (!q.empty()) {
-		int y = q.front().first;
-		int x = q.front().second;
-
-		q.pop();
-
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if (!flag) {
-				if (nx < 0 || ny < 0 || nx >= n || ny >= n || arr[ny][nx] != target || visited2[ny][nx]) {
-					continue;
-				}
-				visited2[ny][nx] = true;
-				q.push(make_pair(ny, nx));
-			}
-			else {
-				if (nx < 0 || ny < 0 || nx >= n || ny >= n || arr[ny][nx] == 'B' || visited2[ny][nx]) {
-					continue;
-				}
-				visited2[ny][nx] = true;
-				q.push(make_pair(ny, nx));
-			}
-		}
-	}
-
-}
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	int answer1 = 0, answer2 = 0;
+    int n;
 
-	cin >> n;
+    int non_cnt = 0;
+    int cnt = 0;
+    queue<pair<int, int> > q;
 
-	arr.resize(n);
+    cin >> n;
 
-	string alpha;
+    vector<string> arr(n);
 
-	for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
-		arr[i].resize(n);
+    vector<vector<int> > non_visited(n, vector<int>(arr[0].size()));
+    vector<vector<int> > visited(n, vector<int>(arr[0].size()));
 
-		cin >> alpha;
+    // 비 적록색약
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < arr[i].size(); j++) {
+            if (non_visited[i][j]) continue;
 
-		for (int j = 0; j < n; j++) {
-			arr[i][j] = alpha[j];
-		}
-	}
+            non_cnt++;
 
+            q.push({i, j});
+            non_visited[i][j] = 1;
 
-	// 한 번 돌 때 2번 돌 수 있도록
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (!visited[i][j]) {
-				bfs(i, j);
-				answer1++;
-			}
-			if (!visited2[i][j]) {
-				bfs2(i, j);
-				answer2++;
-			}
-		}
-	}
+            while (!q.empty()) {
+                int y = q.front().first;
+                int x = q.front().second;
+                q.pop();
+                for (int l = 0; l < 4; l++) {
+                    int ny = y + dy[l];
+                    int nx = x + dx[l];
+                    if (ny < 0 || ny >= n || nx < 0 || nx >= arr[0].size()) continue;
+                    if (non_visited[ny][nx]) continue;
+                    if (arr[i][j] == 'R') {
+                        if (arr[ny][nx] != 'R') continue;
+                    } else if (arr[i][j] == 'B') {
+                        if (arr[ny][nx] != 'B') continue;
+                    } else if (arr[i][j] == 'G') {
+                        if (arr[ny][nx] != 'G') continue;
+                    }
+                    q.push({ny, nx});
+                    non_visited[ny][nx] = 1;
+                }
+            }
+        }
+    }
+    // 적록색약
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < arr[i].size(); j++) {
+            if (visited[i][j]) continue;
+            cnt++;
+            q.push({i, j});
+            visited[i][j] = 1;
+            while (!q.empty()) {
+                int y = q.front().first;
+                int x = q.front().second;
+                q.pop();
+                for (int l = 0; l < 4; l++) {
+                    int ny = y + dy[l];
+                    int nx = x + dx[l];
+                    if (ny < 0 || ny >= n || nx < 0 || nx >= arr[0].size()) continue;
+                    if (visited[ny][nx]) continue;
+                    if (arr[i][j] == 'R' || arr[i][j] == 'G') {
+                        if (arr[ny][nx] == 'B') continue;
+                    } else if (arr[i][j] == 'B') {
+                        if (arr[ny][nx] != 'B') continue;
+                    }
+                    q.push({ny, nx});
+                    visited[ny][nx] = 1;
+                }
+            }
+        }
+    }
 
-	cout << answer1 << ' ' << answer2 << '\n';
+    cout << non_cnt << ' ' << cnt;
 
     return 0;
 }
