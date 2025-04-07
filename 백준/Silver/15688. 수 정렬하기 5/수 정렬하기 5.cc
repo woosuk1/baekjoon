ffcda3c2cf12fcd80b832a7b4b15db51d5217abb
Woosuk Ki <74864092+woosuk1@users.noreply.github.com>
2025-04-08 00:53:18 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 4176 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
+#include <climits>
+#include <queue>
+#include <set>
+#include <math.h>
+#include <stack>
+#include <deque>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+
+    for (auto i: arr)
+        cout << i << '\n';
+
+    return 0;
+}
