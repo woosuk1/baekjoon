13c56c8bbc1c6c48ba10d1a7ef93941b65edd1b8
Woosuk Ki <74864092+woosuk1@users.noreply.github.com>
2025-04-17 17:11:51 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.00 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.py
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.py
@@ -1,14 +1,3 @@
 def solution(num_list, n):
-    answer = []
-    
-    temp = []
-    for i in range(n):
-        temp.append(num_list[i])
-    
-    for i in range(n, len(num_list)):
-        answer.append(num_list[i])
-        
-    answer += temp
-        
-    
-    return answer
+
+    return num_list[n:] + num_list[:n]
