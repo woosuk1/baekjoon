9506b43435a5d0554925137bc4e038f43f300dd9
Woosuk Ki <74864092+woosuk1@users.noreply.github.com>
2025-04-18 01:15:39 +0900

    [level 1] Title: 햄버거 만들기, Time: 190.75 ms, Memory: 25.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,13 @@
+def solution(ingredient):
+    answer = 0
+    
+#     1 2 3 1
+    stack = []
+
+    for i in ingredient:
+        stack.append(i)
+        if stack[-4:] == [1,2,3,1]:
+            answer += 1
+            del stack[-4:]
+    
+    return answer
