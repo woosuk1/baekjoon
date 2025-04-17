4e1333f1280ecc0ae511b78487627a645a3e545a
Woosuk Ki <74864092+woosuk1@users.noreply.github.com>
2025-04-17 16:55:20 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.09 ms, Memory: 9.35 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
@@ -0,0 +1,14 @@
+def solution(my_string):
+    answer = []
+    
+    result = my_string.split(' ')
+    
+    # print(result)
+    
+    for i in result:
+        if i != '':
+            answer.append(i)
+
+
+    
+    return answer
