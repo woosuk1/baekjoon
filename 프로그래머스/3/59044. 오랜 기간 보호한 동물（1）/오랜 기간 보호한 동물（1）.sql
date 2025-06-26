ec8d7ece8d6647d59459f2e20eda3980f7ff2a12
Woosuk Ki <74864092+woosuk1@users.noreply.github.com>
2025-06-26 23:49:50 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+-- 아직 입양을 못 간 동물 중, 가장 오래 보호소에 있었던 동물 3마리의 이름과 보호 시작일을 조회
+SELECT name, datetime
+  from animal_ins
+  where animal_id not in (select animal_id from animal_outs)
+  order by datetime 
+  limit 3;
