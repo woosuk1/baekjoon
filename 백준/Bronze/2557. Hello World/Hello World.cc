40e2d009f1386843d22519aed2ecec9454a3e457
Woosuk Ki <74864092+woosuk1@users.noreply.github.com>
2026-03-29 00:02:21 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
+#include <climits>
+#include <queue>
+#include <set>
+#include <math.h>
+#include <stack>
+#include <deque>
+#define ll long long
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cout << "Hello World!";
+
+	return 0;
+}
